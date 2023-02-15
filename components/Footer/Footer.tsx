38257e25ddca1ba464639f35b9a0694d6708c71c
Philip <philipkarlson77@gmail.com>
2023-02-15 22:00:47 +0800

    nft-marketplace-frontend

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import FooterSocialMedia from "./FooterSocialMedia";
+import { FaFacebook, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import { IconType } from "react-icons";
+
+function Footer() {
+  interface socialMediaListStruct {
+    icon: IconType;
+    url: string;
+  }
+  const socialMediaList: socialMediaListStruct[] = [
+    {
+      icon: FaLinkedin,
+      url: "https://in.linkedin.com/in/nayanrdeveloper",
+    },
+    {
+      icon: FaTwitter,
+      url: "https://twitter.com/nayan_radadiya6",
+    },
+    {
+      icon: FaGithub,
+      url: "https://github.com/nayanrdeveloper",
+    },
+  ];
+  return (
+    <footer className="px-3 md:px-16 py-4 md:py-8 border-t border-[#ffffff14]">
+      <div className="flex flex-col md:flex-row justify-between text-[#acacac]">
+        <div className="flex flex-col justify-center items-center md:flex-row gap-2">
+          <span className="border-r border-[#ffffff14] px-2">
+            ©2022 Nuron, Inc. All rights reserved
+          </span>
+          <ul className="flex gap-2">
+            <li>Terms</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="flex justify-center">
+          <ul className="flex gap-2">
+              {
+                socialMediaList.map((socialMedia: socialMediaListStruct, index) => {
+                  return <FooterSocialMedia {...socialMedia} key={index} />
+                })
+              }
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
